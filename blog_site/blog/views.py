97669580c59c81a565f52cd213d7e86f272dcb3c
sreeharij <sreeharij2003@gmail.com>
2023-07-12 13:50:27 +0530

    add template,static files,applied html inheritance

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author' : 'Sreehari J',
+        'title'  : 'Blog Post 1',
+        'content' : 'First Post Content',
+        'date_posted' : 'July 12, 2023'
+    },
+        {
+        'author' : 'Vishnu TS',
+        'title'  : 'Blog Post 2',
+        'content' : 'Second Post Content',
+        'date_posted' : 'July 13, 2023'
+    },
+        {
+        'author' : 'Ajay Suresh',
+        'title'  : 'Blog Post 3',
+        'content' : 'Third Post Content',
+        'date_posted' : 'July 14, 2023'
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Blog Home</h1>")
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse("<h1>About section of Blog</h1>")
+    return render(request,'blog/about.html',{ 'title':'About' })
