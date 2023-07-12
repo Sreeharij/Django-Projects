a9e04105b79152fed275a7dfe0ce2204edb03a73
sreeharij <sreeharij2003@gmail.com>
2023-07-12 18:17:29 +0530

    day 2:create model,register in admin site,use db

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,30 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'Sreehari J',
-        'title'  : 'Blog Post 1',
-        'content' : 'First Post Content',
-        'date_posted' : 'July 12, 2023'
-    },
-        {
-        'author' : 'Vishnu TS',
-        'title'  : 'Blog Post 2',
-        'content' : 'Second Post Content',
-        'date_posted' : 'July 13, 2023'
-    },
-        {
-        'author' : 'Ajay Suresh',
-        'title'  : 'Blog Post 3',
-        'content' : 'Third Post Content',
-        'date_posted' : 'July 14, 2023'
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
